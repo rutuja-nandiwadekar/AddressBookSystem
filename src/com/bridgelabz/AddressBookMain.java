ddba6b39b5dfca2c71b3f860be18b2e4cf755e52
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-18 22:40:00 +0530

    Added: Edit existing contact person using their name logic

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -30,8 +30,67 @@ public class AddressBookMain {
 		add.email = sc.next();
 		System.out.println("Added Successfully");
 
-		// Add the above list to to person array list
+		// Add the above list to personDetails array list
 		personDetails.add(add);
+
+		System.out.println(add);
+	}
+
+	/*
+	 * This is the method to edit existing contact person using their name
+	 */
+	public void editPerson() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter name to Edit");
+		String name = sc.next();
+
+		for (int i = 0; i < personDetails.size(); i++) {
+			Contacts p = (Contacts) personDetails.get(i);
+
+			if (name.equals(p.getFirstName())) {
+				System.out.println(p);
+
+				System.out.print(
+						"Enter what to update(1.first name/ 2.last name/ 3.city/ 4.state/ 5.phone/ 6.zip/ 7.email): ");
+				sc.nextLine();
+				Integer choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.println("Enter your first name to update:");
+					p.firstName = sc.next();
+					break;
+				case 2:
+					System.out.println("Enter your last name to update:");
+					p.lastName = sc.next();
+					break;
+				case 3:
+					System.out.println("Enter your city name to update:");
+					p.city = sc.next();
+					break;
+				case 4:
+					System.out.println("Enter your state name to update:");
+					p.state = sc.next();
+					break;
+				case 5:
+					System.out.println("Enter Your phone no to update:");
+					p.phNo = sc.nextInt();
+					break;
+				case 6:
+					System.out.println("Enter Your zip to update:");
+					p.zip = sc.nextInt();
+					break;
+				case 7:
+					System.out.println("Enter Your email to update:");
+					p.email = sc.next();
+					break;
+				}
+				// updated personDetails array list
+				personDetails.set(i, p);
+				System.out.println(p);
+			}
+		}
+		sc.close();
 	}
 
 	public static void main(String[] args) {
@@ -39,13 +98,15 @@ public class AddressBookMain {
 		Scanner sc = new Scanner(System.in);
 
 		AddressBookMain addPerson = new AddressBookMain();
+		// to add a new Contact
 		System.out.println("Enter Number of persons you want to add : ");
 		int person_count = sc.nextInt();
 		for (int i = 0; i < person_count; i++) {
 			addPerson.newContact();
 		}
+		// to edit existing contact
+		addPerson.editPerson();
+
 		sc.close();
-
 	}
-
 }
