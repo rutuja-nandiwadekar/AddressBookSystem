6a04f55b48eeb03524a5cdcf6b90c567465dd15c
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-24 10:31:20 +0530

    Added: Add multiple persons to address book logic

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,10 +1,12 @@
+/*
+ *Ability to add multiple person to Address Book
+ */
 package com.bridgelabz;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
-
 	static Scanner sc = new Scanner(System.in);
 	// creation of arraylist
 	ArrayList<Contacts> personDetails = new ArrayList<>();
@@ -15,19 +17,19 @@ public class AddressBookMain {
 	public void newContact() {
 		Contacts add = new Contacts();
 		System.out.println("Enter your First Name");
-		add.firstName = sc.nextLine();
+		add.setFirstName(sc.next());
 		System.out.println("Enter your Last Name");
-		add.lastName = sc.nextLine();
+		add.setLastName(sc.next());
 		System.out.println("Enter your City");
-		add.city = sc.nextLine();
+		add.setCity(sc.next());
 		System.out.println("Enter your state");
-		add.state = sc.nextLine();
+		add.setState(sc.next());
 		System.out.println("Enter your Zip");
-		add.zip = sc.nextInt();
+		add.setZip(sc.nextInt());
 		System.out.println("Enter your Phone Number");
-		add.phNo = sc.nextInt();
+		add.setPhNo(sc.next());
 		System.out.println("Enter your email ID");
-		add.email = sc.next();
+		add.setEmail(sc.next());
 		System.out.println("Added Successfully");
 
 		// Add the above list to personDetails array list
@@ -46,45 +48,45 @@ public class AddressBookMain {
 			Contacts p = (Contacts) personDetails.get(i);
 
 			if (name.equals(p.getFirstName())) {
-				System.out.println(p);
 
 				System.out.print(
 						"Enter what to update(1.first name/ 2.last name/ 3.city/ 4.state/ 5.phone/ 6.zip/ 7.email): ");
-				sc.nextLine();
-				Integer choice = sc.nextInt();
+				int choice = sc.nextInt();
 				switch (choice) {
 				case 1:
 					System.out.println("Enter your first name to update:");
-					p.firstName = sc.next();
+					p.setFirstName(sc.next());
 					break;
 				case 2:
 					System.out.println("Enter your last name to update:");
-					p.lastName = sc.next();
+					p.setLastName(sc.next());
 					break;
 				case 3:
 					System.out.println("Enter your city name to update:");
-					p.city = sc.next();
+					p.setCity(sc.next());
 					break;
 				case 4:
 					System.out.println("Enter your state name to update:");
-					p.state = sc.next();
+					p.setState(sc.next());
 					break;
 				case 5:
 					System.out.println("Enter Your phone no to update:");
-					p.phNo = sc.nextInt();
+					p.setPhNo(sc.next());
 					break;
 				case 6:
 					System.out.println("Enter Your zip to update:");
-					p.zip = sc.nextInt();
+					p.setZip(sc.nextInt());
 					break;
 				case 7:
 					System.out.println("Enter Your email to update:");
-					p.email = sc.next();
+					p.setEmail(sc.next());
+					break;
+				default:
+					System.out.println("invalid choice");
 					break;
 				}
-				// updated personDetails array list
 				personDetails.set(i, p);
-				System.out.println(p);
+				System.out.println("Contacts Edited Successfully\n" + personDetails);
 			}
 		}
 	}
@@ -100,22 +102,49 @@ public class AddressBookMain {
 		for (int i = 0; i < personDetails.size(); i++) {
 			Contacts p = (Contacts) personDetails.get(i);
 			if (name.equals(p.getFirstName())) {
-				System.out.println(p);
 				personDetails.remove(i);
-				System.out.println(personDetails.isEmpty());
+				System.out.println("Contacts deleted Successfully\n" + personDetails);
 			}
 		}
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program ");
+	/*
+	 * This is the method to view existing contact details
+	 */
+	public void viewPerson() {
+		for (Contacts p : personDetails) {
+			System.out.println(p);
+		}
+	}
 
+	// main method
+	public static void main(String args[]) {
+		int choice = 1, person_count, i;
+		// Creating the object of the class
 		AddressBookMain addPerson = new AddressBookMain();
-		// to add a new Contact
-		addPerson.newContact();
-		// to edit existing contact
-		addPerson.editPerson();
-		// to delete existing contact
-		addPerson.deletePerson();
+		while (choice != 0) {
+			System.out.println("1.Add Person\n2.Edit Person\n3.Delete Person\n4.Display Person\nEnter your choice:\n");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Enter Number of persons you want to add :");
+				person_count = sc.nextInt();
+				for (i = 0; i < person_count; i++) {
+					addPerson.newContact();
+				}
+				break;
+			case 2:
+				addPerson.editPerson();
+				break;
+			case 3:
+				addPerson.deletePerson();
+				break;
+			case 4:
+				addPerson.viewPerson();
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -3,109 +3,68 @@ package com.bridgelabz;
 public class Contacts {
 
 	// initialization
-	String firstName;
-	String lastName;
-	String city;
-	String state;
-	int zip;
-	int phNo;
-	String email;
+	private String firstName;
+	private String lastName;
+	private String city;
+	private String state;
+	private int zip;
+	private String phNo;
+	private String email;
 
 	// generating getter and setter
-	/**
-	 * @return the firstName
-	 */
+
 	public String getFirstName() {
 		return firstName;
 	}
 
-	/**
-	 * @param firstName the firstName to set
-	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-	/**
-	 * @return the lastName
-	 */
 	public String getLastName() {
 		return lastName;
 	}
 
-	/**
-	 * @param lastName the lastName to set
-	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	/**
-	 * @return the city
-	 */
 	public String getCity() {
 		return city;
 	}
 
-	/**
-	 * @param city the city to set
-	 */
 	public void setCity(String city) {
 		this.city = city;
 	}
 
-	/**
-	 * @return the state
-	 */
 	public String getState() {
 		return state;
 	}
 
-	/**
-	 * @param state the state to set
-	 */
 	public void setState(String state) {
 		this.state = state;
 	}
 
-	/**
-	 * @return the zip
-	 */
 	public int getZip() {
 		return zip;
 	}
 
-	/**
-	 * @param zip the zip to set
-	 */
 	public void setZip(int zip) {
 		this.zip = zip;
 	}
 
-	/**
-	 * @return the phNo
-	 */
-	public int getPhNo() {
+	public String getPhNo() {
 		return phNo;
 	}
 
-	/**
-	 * @param phNo the phNo to set
-	 */
-	public void setPhNo(int phNo) {
+	public void setPhNo(String phNo) {
 		this.phNo = phNo;
 	}
 
-	/**
-	 * @return the email
-	 */
 	public String getEmail() {
 		return email;
 	}
 
-	/**
-	 * @param email the email to set
-	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
